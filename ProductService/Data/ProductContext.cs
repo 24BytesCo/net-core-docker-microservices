9a72de65d9b127bc4269b845dbc8220f638bb897
Alexis Dorado Muñoz <alexis.dorado.munoz@gmail.com>
2025-02-09 12:11:33 -0500

    Feature: Se Implementa API con crud  básico de productos con Entity Framework Core
    - Se implementa la API de productos con Entity Framework Core, incluyendo las operaciones CRUD básicas (Crear, Leer, Actualizar, Eliminar).
    - Se define el modelo de datos `Product` con propiedades como `Id`, `Name` y `Price`.
    - Se configura el contexto de base de datos `ProductContext` para interactuar con la base de datos.

--- ProductService/Data/ProductContext.cs
+++ ProductService/Data/ProductContext.cs
@@ -2,11 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
 
 namespace ProductService.Data
 {
-    public class ProductContext
+    public class ProductContext: DbContext
     {
-        
+        public ProductContext(DbContextOptions<ProductContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().
+            Property(p=> p.Price).
+            HasColumnType("decimal(18,2)");
+        }
     }
 }
--- ProductService/Models/Product.cs
+++ ProductService/Models/Product.cs
@@ -5,8 +5,26 @@ using System.Threading.Tasks;
 
 namespace ProductService.Models
 {
+    /// <summary>
+    /// Representa un producto en el sistema.
+    /// </summary>
     public class Product
     {
+        /// <summary>
+        /// Identificador único del producto.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre del producto.
+        /// Campo es obligatorio.
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Precio del producto.
+        /// </summary>
+        public decimal Price { get; set; }
         
     }
 }
